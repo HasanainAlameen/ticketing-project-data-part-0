7e268bd67f799c787390542ec00b6c6cb1873860
Hasanain_Cydeo <hasanainalameen@gmail.com>
2023-04-25 10:06:04 -0500

    Ticketing project - Task part

--- ticketing-project-data-part-0/src/main/java/com/cydeo/service/ProjectService.java
+++ ticketing-project-data-part-0/src/main/java/com/cydeo/service/ProjectService.java
@@ -14,6 +14,7 @@ public interface ProjectService {
     void update(ProjectDTO dto);
     void delete(String code);
     void complete(String projectCode);
+    List<ProjectDTO> listAllProjectsDetails();
 
 
 
